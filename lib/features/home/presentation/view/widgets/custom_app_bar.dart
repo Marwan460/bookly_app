fbe2eb3e7ad189517e7bdbe9ca438f092476201f
Marwan <marwanyousry52@gmail.com>
2025-01-20 04:30:02 +0200

    Add custom appbar to home.

---
+++ lib/features/home/presentation/view/widgets/custom_app_bar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utils/app_assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 44),
+      child: Row(
+        children: [
+          Image.asset(AppAssets.logo,height: 20,),
+          const Spacer(),
+          IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.magnifyingGlass,size: 24,)),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+import 'custom_app_bar.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomAppBar(),
+        ]
+    );
+  }
+}
