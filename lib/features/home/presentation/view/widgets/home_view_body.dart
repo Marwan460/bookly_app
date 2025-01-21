70e3344a5194238d2a038e16d5c5863b0cb3b0df
Marwan <marwanyousry52@gmail.com>
2025-01-21 03:54:43 +0200

    Add text to home & font family.

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/featured_books_list_view.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -8,6 +9,21 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [CustomAppBar(), FeaturedBooksListView()]);
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+      CustomAppBar(),
+      Padding(
+        padding: EdgeInsets.only(left: 20),
+        child: FeaturedBooksListView(),
+      ),
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Text(
+          "Best Seller",
+          style: Styles.titleMedium,
+        ),
+      )
+    ]);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:bookly_app/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'features/splash/presentation/view/splash_view.dart';
-
 
 void main() {
   runApp(const Bookly());
@@ -17,9 +17,9 @@ class Bookly extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: AppColors.primary),
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: AppColors.primary, textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       home: const SplashView(),
     );
   }
 }
-
