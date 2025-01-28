8532c8c76ac4332be4ff4a11742e6ad9c156c04d
Marwan <marwanyousry52@gmail.com>
2025-01-29 01:51:39 +0200

    Add custom scroll view to book details view.

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/features/home/presentation/view/widgets/books_details_section.dart';
-import 'package:bookly_app/features/home/presentation/view/widgets/custom-book_details_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
@@ -8,23 +8,30 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: CustomBookDetailsAppBar(),
+    return const CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: CustomBookDetailsAppBar(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              BooksDetailsSection(),
+              Expanded(
+                child: SizedBox(
+                  height: 50,
+                ),
+              ),
+              SimilarBooksSection(),
+            ],
           ),
-          SizedBox(
-            height: 30,
-          ),
-          BooksDetailsSection(),
-          SizedBox(
-            height: 50,
-          ),
-          SimilarBooksSection(),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
