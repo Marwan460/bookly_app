a7a6a5deeb13417154ab67a92ee2e38361fd2a3d
Marwan <marwanyousry52@gmail.com>
2025-01-28 23:39:00 +0200

    Create similar books list view.

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/features/home/presentation/view/widgets/book_rating.d
 import 'package:bookly_app/features/home/presentation/view/widgets/books_action.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/custom-book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_image.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -11,45 +12,52 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.sizeOf(context).width;
-    return Column(
-      children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: CustomBookDetailsAppBar(),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * 0.28),
-          child: const CustomBookImage(),
-        ),
-        const SizedBox(
-          height: 43,
-        ),
-        const Text(
-          "The Jungle Book",
-          style: Styles.textStyle30,
-        ),
-        const SizedBox(
-          height: 6,
-        ),
-        Opacity(
-          opacity: 0.7,
-          child: Text(
-            "Rudyard Kipling",
-            style: Styles.textStyle18.copyWith(
-                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: CustomBookDetailsAppBar(),
           ),
-        ),
-        const SizedBox(
-          height: 18,
-        ),
-        const BookRating(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-        const SizedBox(
-          height: 37,
-        ),
-        const BooksAction(),
-      ],
+          const SizedBox(height: 30,),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 0.3),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            "The Jungle Book",
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: 0.7,
+            child: Text(
+              "Rudyard Kipling",
+              style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          const SizedBox(
+            height: 37,
+          ),
+          const BooksAction(),
+          const SizedBox(height: 50,),
+          Text("You can also like",style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),),
+          const SizedBox(height: 16,),
+          const SimilarBooksListView()
+        ],
+      ),
     );
   }
 }
---
+++ lib/features/home/presentation/view/widgets/similar_books_list_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'custom_book_image.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * 0.2,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.only(bottom: 40,left: 5, right: 5),
+              child: CustomBookImage(),
+            );
+          }),
+    );
+  }
+}
