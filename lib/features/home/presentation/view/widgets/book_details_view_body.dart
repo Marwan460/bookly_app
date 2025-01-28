d2f2c0e81e19c736384bee42c53415a7f10ce1e6
Marwan <marwanyousry52@gmail.com>
2025-01-28 03:45:12 +0200

    Add title and subtitle to booK details view.

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/custom-book_details_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_item.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -8,7 +9,7 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.sizeOf(context).width;
-    return  Column(
+    return Column(
       children: [
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 30),
@@ -18,6 +19,24 @@ class BookDetailsViewBody extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: width * 0.28),
           child: const CustomBookImage(),
         ),
+        const SizedBox(
+          height: 43,
+        ),
+        const Text(
+          "The Jungle Book",
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            "Rudyard Kipling",
+            style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+          ),
+        )
       ],
     );
   }
